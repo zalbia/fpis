9abe1f966245047be0c061af11ad91540a38810f
Zach Albia <zach.albia@gmail.com>
2021-03-12 15:14:00 +0800

    add unsafeFoldRight, sum2 & product2

--- src/main/scala/part1/chapter3/List.scala
+++ src/main/scala/part1/chapter3/List.scala
@@ -1,29 +1,15 @@
 package part1.chapter3
 
-sealed trait List[+A]
-case object Nil extends List[Nothing]
-case class Cons[A](head: A, tail: List[A]) extends List[A]
-
-object List {
-  def sum(ints: List[Int]): Int = ints match {
-    case Nil => 0
-    case Cons(x, xs) => x + sum(xs)
+sealed trait List[+A] { self =>
+  def unsafeFoldRight[B](z: B)(f: (A, B) => B): B = self match {
+    case Nil => z
+    case Cons(x, xs) =>f(x, xs.unsafeFoldRight(z)(f))
   }
 
-  def product(ds: List[Double]): Double = ds match {
-    case Nil => 0
-    case Cons(0.0, _) => 0.0
-    case Cons(x, xs) => x * product(xs)
-  }
+  def iterator: Iterator[A] = new Iterator[A] {
+    var list = self
 
-  def apply[A](as: A*): List[A] =
-    if (as.isEmpty) Nil
-    else Cons(as.head, apply(as.tail: _*))
-
-  def iterator[A](as: List[A]): Iterator[A] = new Iterator[A] {
-    var list = as
-
-    def hasNext: Boolean = as match {
+    def hasNext: Boolean = list match {
       case Nil => false
       case _ => true
     }
@@ -35,4 +21,29 @@ object List {
         head
     }
   }
+}
+case object Nil extends List[Nothing]
+case class Cons[A](head: A, tail: List[A]) extends List[A]
+
+object List {
+  def apply[A](as: A*): List[A] =
+    if (as.isEmpty) Nil
+    else Cons(as.head, apply(as.tail: _*))
+
+  def sum(ints: List[Int]): Int = ints match {
+    case Nil => 0
+    case Cons(x, xs) => x + sum(xs)
+  }
+
+  def sum2(ints: List[Int]): Int =
+    ints.unsafeFoldRight(0)(_ + _)
+
+  def product(ds: List[Double]): Double = ds match {
+    case Nil => 0
+    case Cons(0.0, _) => 0.0
+    case Cons(x, xs) => x * product(xs)
+  }
+
+  def product2(ds :List[Double]): Double =
+    ds.unsafeFoldRight(1.0)(_ * _)
 }
